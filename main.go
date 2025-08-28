49fe39e418faf237b4c67c2176b779c57e047673
Ege Güneş <ege.gunes@percona.com>
2025-08-28 16:51:41 +0300

    add reconnect option

--- main.go
+++ main.go
@@ -29,6 +29,8 @@ type config struct {
 	readTimeout  int32
 
 	tickerSeconds int32
+
+	reconnect bool
 }
 
 func (c config) MySQLUri(db string) string {
@@ -44,13 +46,14 @@ func (c config) MySQLUri(db string) string {
 }
 
 func main() {
-	var initialize bool
+	var initialize, reconnect bool
 	var hostname, username, password, database, table string
 	var readers, writers, port int
 	var readTimeout, writeTimeout, dialTimeout int
 	var tickerSeconds int
 
 	flag.BoolVar(&initialize, "i", false, "Initialize DB")
+	flag.BoolVar(&reconnect, "rc", false, "Reconnect")
 
 	flag.StringVar(&hostname, "h", "127.0.0.1", "Host")
 	flag.StringVar(&username, "u", "user", "Username")
@@ -79,6 +82,7 @@ func main() {
 		writeTimeout:  int32(writeTimeout),
 		dialTimeout:   int32(dialTimeout),
 		tickerSeconds: int32(tickerSeconds),
+		reconnect:     reconnect,
 	}
 
 	log.Printf("DSN: %s", cfg.MySQLUri(cfg.database))
@@ -120,10 +124,10 @@ func main() {
 	wg.Wait()
 }
 
-func initalizeDB(ctx context.Context, cfg config) error {
-	db, err := sql.Open("mysql", cfg.MySQLUri("mysql"))
+func connectToDB(ctx context.Context, cfg config, database string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", cfg.MySQLUri(database))
 	if err != nil {
-		return fmt.Errorf("failed to connect to db: %w", err)
+		return db, fmt.Errorf("failed to connect to db: %w", err)
 	}
 	db.SetConnMaxLifetime(time.Second * 10)
 	db.SetConnMaxIdleTime(time.Second * 5)
@@ -131,7 +135,16 @@ func initalizeDB(ctx context.Context, cfg config) error {
 	db.SetMaxIdleConns(10)
 
 	if err := db.PingContext(ctx); err != nil {
-		return fmt.Errorf("failed to ping: %w", err)
+		return db, fmt.Errorf("failed to ping: %w", err)
+	}
+
+	return db, nil
+}
+
+func initalizeDB(ctx context.Context, cfg config) error {
+	db, err := connectToDB(ctx, cfg, "mysql")
+	if err != nil {
+		return fmt.Errorf("failed to connect to db: %w", err)
 	}
 
 	_, err = db.ExecContext(ctx, fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", cfg.database))
@@ -163,18 +176,14 @@ func randSeq(n int) string {
 }
 
 func writer(ctx context.Context, id int, cfg config) {
-	db, err := sql.Open("mysql", cfg.MySQLUri(cfg.database))
-	if err != nil {
-		log.Printf("writer %d: failed to connect to db: %v", id, err)
-	}
-	db.SetConnMaxLifetime(time.Second * 10)
-	db.SetConnMaxIdleTime(time.Second * 5)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
-
-	if err := db.PingContext(ctx); err != nil {
-		log.Printf("writer %d: failed to ping: %v", id, err)
-		return
+	var db *sql.DB
+	var err error
+	if !cfg.reconnect {
+		db, err = connectToDB(ctx, cfg, cfg.database)
+		if err != nil {
+			log.Printf("writer %d: failed to connect to db: %v", id, err)
+			return
+		}
 	}
 
 	ticker := time.NewTicker(time.Duration(cfg.tickerSeconds) * time.Second)
@@ -183,6 +192,15 @@ func writer(ctx context.Context, id int, cfg config) {
 		if errors.Is(ctx.Err(), context.Canceled) {
 			log.Printf("writer %d: shutting down", id)
 			return
+		}
+
+		if cfg.reconnect {
+			log.Printf("writer %d: connecting to db", id)
+			db, err = connectToDB(ctx, cfg, cfg.database)
+			if err != nil {
+				log.Printf("writer %d: failed to connect to db: %v", id, err)
+				continue
+			}
 		}
 
 		var hostname string
@@ -202,18 +220,14 @@ func writer(ctx context.Context, id int, cfg config) {
 }
 
 func reader(ctx context.Context, id int, cfg config) {
-	db, err := sql.Open("mysql", cfg.MySQLUri(cfg.database))
-	if err != nil {
-		log.Printf("reader %d: failed to connect to db: %v", id, err)
-	}
-	db.SetConnMaxLifetime(time.Second * 10)
-	db.SetConnMaxIdleTime(time.Second * 5)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
-
-	if err := db.PingContext(ctx); err != nil {
-		log.Printf("reader %d: failed to ping: %v", id, err)
-		return
+	var db *sql.DB
+	var err error
+	if !cfg.reconnect {
+		db, err = connectToDB(ctx, cfg, cfg.database)
+		if err != nil {
+			log.Printf("reader %d: failed to connect to db: %v", id, err)
+			return
+		}
 	}
 
 	ticker := time.NewTicker(time.Duration(cfg.tickerSeconds) * time.Second)
@@ -222,6 +236,15 @@ func reader(ctx context.Context, id int, cfg config) {
 		if errors.Is(ctx.Err(), context.Canceled) {
 			log.Printf("reader %d: shutting down", id)
 			return
+		}
+
+		if cfg.reconnect {
+			log.Printf("reader %d: connecting to db", id)
+			db, err = connectToDB(ctx, cfg, cfg.database)
+			if err != nil {
+				log.Printf("reader %d: failed to connect to db: %v", id, err)
+				continue
+			}
 		}
 
 		var hostname string
